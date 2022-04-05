38d30ac2db10e0c192e5a8eadb037cda8909b580
KewryX <mate.agfay@gmail.com>
2022-04-05 11:05:03 +0200

    coopweblap_design_nelkul

--- APP/static/js/script.js
+++ APP/static/js/script.js
@@ -1,5 +1,23 @@
-document.addEventListener("DOMContentLoaded", main);
+document.addEventListener('DOMContentLoaded', main)
 
-function main(){
-    alert("Látni fogsz egydagadt macskát! Elég tömött egy fiú, ne bántsd miatta, köszi !:3");
+async function main(){
+
+
+    let jokediv = document.querySelector('#joke');
+    //macskafaktdiv.innerHTML="X";
+
+    let url = "https://v2.jokeapi.dev/joke/Any?safe-mode";
+    let szotar = await olvaso_fetch(url);
+    console.log(szotar['type']);
+    console.log(szotar['joke']);
+
+    jokediv.innerHTML=szotar.joke;
+
+}
+
+
+async function olvaso_fetch(url){
+    let promise = await fetch(url);
+    let promise_json = await promise.json();
+    return promise_json;
 }
